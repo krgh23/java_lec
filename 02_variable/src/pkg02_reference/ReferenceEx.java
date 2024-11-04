4dd48b49b4c0ed3315bfd2373653950be72877c7
krgh23 <krgh23@naver.com>
2024-11-04 16:52:19 +0900

    20241104

--- 02_variable/src/pkg02_reference/ReferenceEx.java
+++ 02_variable/src/pkg02_reference/ReferenceEx.java
@@ -8,7 +8,7 @@ public class ReferenceEx {
     
     // String 클래스 : 주소 값(참조 값)을 저장하는 대표 참조 타입, 문자열 저장
     String s = "Hello World";
-    System.out.println(s);                          // 실제로는 문자열 대신 참조값이 들어가 있지만 자바에서는 문자열을 보여줌
+    System.out.println(s);                          // 실제로는 문자열 대신 참조값이 들어가 있지만 자바에서는 참조값을 바탕으로 문자열을 찾아서 보여줌
     System.out.println(System.identityHashCode(s)); // "Hello World" 문자열이 저장된 메모리의 참조값을 찾아 출력
 
      // ReferenceEx 클래스 타입 (6장부터 다룰 예정)
