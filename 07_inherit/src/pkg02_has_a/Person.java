599e5fab95467144ec30efd76645ba555a7be44a
krgh23 <krgh23@naver.com>
2024-11-08 14:36:51 +0900

    20241108

--- 07_inherit/src/pkg02_has_a/Person.java
+++ 07_inherit/src/pkg02_has_a/Person.java
@@ -1,7 +1,23 @@
 package pkg02_has_a;
 
+/*
+  ┌--------------┐
+  │      Pen     │ 슈퍼 클래스
+  │--------------│
+  │   drawing()  │
+  └--------------┘
+          ▲
+          │
+  ┌--------------┐
+  │    Person    │ 서브 클래스
+  │--------------│
+  │     eat()    │
+  │    sleep()   │
+  └--------------┘
+*/
+
 public class Person extends Pen {
-  
+
   public void eat() {
     System.out.println("먹는다.");
   }
